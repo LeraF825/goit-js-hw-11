c585cae7e33dd97112fd2f2139e0ebfd5bfe9dee
LeraF825 <fertikovalera@gmail.com>
2023-05-10 17:15:04 +0300

    доработать стили и скролл

--- src/fetchImage.js
+++ src/fetchImage.js
@@ -3,19 +3,20 @@ import axios from 'axios';
 export class ImagesAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '36214859-9a36ec77f3d5f2e4fe5aa554f';
-  #PAGE = 1;
-  #PER_PAGE = 40;
+  PAGE = 1;
+  PER_PAGE = 40;
+  QUERY = '';
 
-  async getImages(query) {
+  async getImages() {
     const url = this.#BASE_URL;
     const params = new URLSearchParams({
       key: this.#API_KEY,
-      q: query,
+      q: this.QUERY,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
-      per_page: this.#PER_PAGE,
-      page: this.#PAGE,
+      per_page: this.PER_PAGE,
+      page: this.PAGE,
     });
     try {
       const response = await axios.get(`${url}?${params}`);
@@ -26,3 +27,4 @@ export class ImagesAPI {
     }
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
+import './styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { ImagesAPI } from './fetchImage';
 
+
 const formElem = document.querySelector('#search-form');
 const loadMoreBtn = document.querySelector('.load-more');
-const galleryElem = document.querySelector('.gallery')
+const galleryElem = document.querySelector('.gallery');
 
+hideLoadBtn();
 
 const imagesAPI = new ImagesAPI();
-
 formElem.addEventListener('submit', onFormSubmit);
-// loadMoreBtn.addEventListener('click', onLoadMore);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  let query = e.target.elements.searchQuery.value;
+  hideLoadBtn();
+  let query = e.target.elements.searchQuery.value.trim();
+  imagesAPI.PAGE = 1;
+  imagesAPI.QUERY = query;
   galleryElem.innerHTML = '';
-  imagesAPI.getImages(query).then(data => {
-    console.log(data);
-    renderImages(data.hits);
+ 
+  if (query === '') {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.',
+    );
+    return;
+  }
+
+  imagesAPI.getImages().then(data => {
+    if (data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+    } else {
+      renderImages(data.hits);
+      showLoadBtn();
+      simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }  
+  })
+  .catch(error => console.log(error))
+  .finally(() => {
+    formElem.reset();
   });
 }
 
@@ -36,17 +61,17 @@ function imagesMarkup(hits) {
         downloads,
       }) => {
         return `
-      <a href="${largeImageURL}">
         <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy"  width="250" height="200"/>
           <div class="info">
             <p class="info-item"><b>Likes</b>${likes}</p>
             <p class="info-item"><b>Views</b>${views}</p>
             <p class="info-item"><b>Comments</b>${comments}</p>
             <p class="info-item"><b>Downloads</b>${downloads}</p>
           </div>
+          </a>
         </div>
-      </a>
     `;
       }
     )
@@ -58,11 +83,27 @@ const markup = imagesMarkup(hits);
 galleryElem.insertAdjacentHTML('beforeend',markup)
 }
 
+function onLoadMore(){
+imagesAPI.PAGE++;
+imagesAPI.getImages().then(data => {
+  renderImages(data.hits);  
+  simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+
+  const totalPages = Math.ceil(data.totalHits/imagesAPI.PER_PAGE);
+
+  if(imagesAPI.PAGE >= totalPages){
+    hideLoadBtn();
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results.",
+    );
+  }
+}).catch(error => console.log(error));
+}
 
 function hideLoadBtn(){
-    loadMoreBtn.classList.add('js-hidden');
+  loadMoreBtn.classList.add('js-hidden');
 }
 
 function showLoadBtn(){
-    loadMoreBtn.classList.remove('js-hidden');
+  loadMoreBtn.classList.remove('js-hidden');
 }
